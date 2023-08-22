26d7ec638fa510a6d39dcdcdd7f7289260038343
John <ashton.basile@gmail.com>
2023-08-22 16:07:33 +0100

    The options are now showing

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,7 +1,9 @@
 function Options({ options, handleAnswerSelect }) {
+  const optionItems = options.map((option, index) => <li>{option}</li>);
   return (
     <>
       <h1>Options should go here</h1>
+      <ul>{optionItems}</ul>
     </>
   );
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,7 +3,9 @@ import Options from "./Options";
 function Question({ question, options, handleAnswerSelect }) {
   return (
     <>
-      <Options handleAnswerSelect={handleAnswerSelect} />
+      <h1>Here is where the questions will go</h1>
+      <h3>{question.question}</h3>
+      <Options options={options} handleAnswerSelect={handleAnswerSelect} />
     </>
   );
 }
