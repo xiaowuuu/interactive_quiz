03532e1d302997bc5796e358308a60b7fb5815bb
John <ashton.basile@gmail.com>
2023-08-22 15:41:39 +0100

    I removed some comments

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,13 +1,3 @@
-// ### Options Component
-
-// The `Options` component renders the multiple-choice options for a question and provides interaction for the user to select an answer and proceed to the next question.
-
-// #### Props:
-
-// - **options:** An array of possible answer options for the question.
-
-// - **handleAnswerSelect:** A function passed from the `Question` component that triggers when the user selects an answer.
-
 function Options({ options, handleAnswerSelect }) {
   return (
     <>
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,14 +1,3 @@
-// ### Question Component
-
-// The `Question` component is responsible for displaying a single question and its corresponding options to the user.
-
-// #### Props:
-
-// - **question:** The text of the question to be displayed.
-
-// - **options:** An array of possible answer options for the question.
-
-// - **handleAnswerSelect:** A function passed from the `Quiz` component that handles the user's answer selection.
 import Options from "./Options";
 
 function Question({ question, options, handleAnswerSelect }) {
