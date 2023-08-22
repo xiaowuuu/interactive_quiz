e4e9a45ccaa9e96bfd89618560514026de8cd6a2
John <ashton.basile@gmail.com>
2023-08-22 16:11:43 +0100

    We have a select dropdown

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,9 +1,9 @@
 function Options({ options, handleAnswerSelect }) {
-  const optionItems = options.map((option, index) => <li>{option}</li>);
+  const optionItems = options.map((option, index) => <option>{option}</option>);
   return (
     <>
       <h1>Options should go here</h1>
-      <ul>{optionItems}</ul>
+      <select>{optionItems}</select>
     </>
   );
 }
